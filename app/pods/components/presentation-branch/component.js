b13ee62da1d814ff509ab71f208a3a5ab0beae43
Seosamh Cahill <seo.cahill@gmail.com>
2017-03-14 23:27:53 +0100

    Toggle showing children

--- app/pods/components/presentation-branch/component.js
+++ app/pods/components/presentation-branch/component.js
@@ -1,5 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  tagName: 'li'
+  tagName: 'li',
+  showChildren: false,
+
+  actions: {
+    toggleShowChildren() {
+      this.toggleProperty('showChildren');
+    }
+  }
 });
