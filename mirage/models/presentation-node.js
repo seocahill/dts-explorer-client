7d88ee9b69222ddf62bf40087adec2516d0d8fbd
Seosamh Cahill <seo.cahill@gmail.com>
2017-03-14 21:11:50 +0100

    Children working

--- mirage/models/presentation-node.js
+++ mirage/models/presentation-node.js
@@ -3,5 +3,6 @@ import { Model, belongsTo } from 'ember-cli-mirage';
 export default Model.extend({
   roleType: belongsTo(),
   element: belongsTo(),
-  parent: belongsTo('presentation-node')
+  parent: belongsTo('presentation-node'),
+  // children: belongsTo('presentation-node')
 });
