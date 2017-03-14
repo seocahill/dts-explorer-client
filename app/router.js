ccf3fc076e78815ad56c213631807a3b71538eec
Seosamh Cahill <seo.cahill@gmail.com>
2017-03-14 17:38:31 +0100

    Added DTS resrouce

--- app/router.js
+++ app/router.js
@@ -7,6 +7,8 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('discoverable-taxonomy-sets');
+  this.route('discoverable-taxonomy-set');
 });
 
 export default Router;
