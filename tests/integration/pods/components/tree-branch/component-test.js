465a3e72fbb857982fd95642e1956e45c629414b
Seosamh Cahill <seo.cahill@gmail.com>
2017-03-16 12:31:02 +0100

    Renamed branch component

--- tests/integration/pods/components/tree-branch/component-test.js
+++ tests/integration/pods/components/tree-branch/component-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('presentation-branch', 'Integration | Component | presentation branch', {
+moduleForComponent('tree-branch', 'Integration | Component | tree branch', {
   integration: true
 });
 
@@ -10,15 +10,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{presentation-branch}}`);
+  this.render(hbs`{{tree-branch}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#presentation-branch}}
+    {{#tree-branch}}
       template block text
-    {{/presentation-branch}}
+    {{/tree-branch}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
