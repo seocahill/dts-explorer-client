f2a494c1985ab7a080567ba05d16ee42c7560d86
Seosamh Cahill <seo.cahill@gmail.com>
2017-04-30 17:03:38 +0200

    Fix parent link

--- app/pods/discoverable-taxonomy-set/role-type/presentation-node/route.js
+++ app/pods/discoverable-taxonomy-set/role-type/presentation-node/route.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    console.log("called", params.presentation_node_id)
     return this.get('store').findRecord('presentation-node', params.presentation_node_id)
   }
 });
