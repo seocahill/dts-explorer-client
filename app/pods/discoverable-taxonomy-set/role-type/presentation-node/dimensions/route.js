189d3222b4277f42dd19176fffbfaafb7e4557d1
Seosamh Cahill <seo.cahill@gmail.com>
2017-04-30 18:33:43 +0200

    Need to tidy up dimensions
    But it works anyway

---
+++ app/pods/discoverable-taxonomy-set/role-type/presentation-node/dimensions/route.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+
+const { isBlank } = Ember;
+
+export default Ember.Route.extend({
+  model() {
+    return this.modelFor('discoverable-taxonomy-set.role-type.presentation-node').get('element.dimensionNodes');
+  },
+
+  setupController(controller, model) {
+    this._super(...arguments);
+    controller.set('rootNodes', model.filter((node) => isBlank(node.get('parent.id'))));
+  }
+});
