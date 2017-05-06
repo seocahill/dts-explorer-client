463e8f0ab086655baab2ac25f8eecd8d0b121e83
Seosamh Cahill <seo.cahill@gmail.com>
2017-05-06 18:11:00 +0200

    Remove unused action

--- app/pods/discoverable-taxonomy-set/role-type/controller.js
+++ app/pods/discoverable-taxonomy-set/role-type/controller.js
@@ -5,11 +5,5 @@ const { computed } = Ember;
 export default Ember.Controller.extend({
   rootNodes: computed.filter('model.presentationNodes', function(node) {
     return node.get('parent.id') === undefined; 
-  }),
-
-  actions: {
-    viewElement(element) {
-      this.transitionToRoute('discoverable-taxonomy-set.role-type.element', element);
-    }
-  }
+  })
 });
