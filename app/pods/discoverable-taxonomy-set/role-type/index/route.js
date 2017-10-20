af08a74ab0228e805bd3705ac0a2e3914de2fdbb
Seosamh Cahill <seo.cahill@gmail.com>
2017-10-20 20:25:32 +0200

    Redirect to pnodes from role

--- app/pods/discoverable-taxonomy-set/role-type/index/route.js
+++ app/pods/discoverable-taxonomy-set/role-type/index/route.js
@@ -2,6 +2,6 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   afterModel() {
-    // this.replaceWith('discoverable-taxonomy-set.role-type.presentation-nodes');
+    this.replaceWith('discoverable-taxonomy-set.role-type.presentation-nodes');
   }
 });
