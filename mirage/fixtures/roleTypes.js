fa6bbda1fa9cb3b2304be32251cf83a1d4ce1a93
Seosamh Cahill <seo.cahill@gmail.com>
2017-10-23 19:51:03 +0200

    Fixture id must be unique

--- mirage/fixtures/roleTypes.js
+++ mirage/fixtures/roleTypes.js
@@ -5,87 +5,87 @@ export default [
     "discoverableTaxonomySetId": "1"
   },
   {
-    "id": "1",
+    "id": "2",
     "definition": "02 - Business Report Information",
     "discoverableTaxonomySetId": "1"
   },
   {
-    "id": "1",
+    "id": "5",
     "definition": "05 - Directors' Report",
     "discoverableTaxonomySetId": "1"
   },
   {
-    "id": "1",
+    "id": "6",
     "definition": "06 - Auditor's Report",
     "discoverableTaxonomySetId": "1"
   },
   {
-    "id": "1",
+    "id": "10",
     "definition": "10 - Profit and Loss Account",
     "discoverableTaxonomySetId": "1"
   },
   {
-    "id": "1",
+    "id": "11",
     "definition": "11 - Balance Sheet",
     "discoverableTaxonomySetId": "1"
   },
   {
-    "id": "1",
+    "id": "12",
     "definition": "12 - Cash Flow Statement",
     "discoverableTaxonomySetId": "1"
   },
   {
-    "id": "1",
+    "id": "13",
     "definition": "13 - Statement of Total Recognised Gains and Losses",
     "discoverableTaxonomySetId": "1"
   },
   {
-    "id": "1",
+    "id": "14",
     "definition": "14 - Note of Historical Cost Profits and Losses",
     "discoverableTaxonomySetId": "1"
   },
   {
-    "id": "1",
+    "id": "15",
     "definition": "15 - Reconciliation of Movements in Shareholder Funds",
     "discoverableTaxonomySetId": "1"
   },
   {
-    "id": "1",
+    "id": "20",
     "definition": "20 - Notes and Detailed Disclosures",
     "discoverableTaxonomySetId": "1"
   },
   {
-    "id": "1",
+    "id": "30",
     "definition": "30 - Detailed Profit and Loss",
     "discoverableTaxonomySetId": "1"
   },
   {
-    "id": "1",
+    "id": "94",
     "definition": "94 - Countries and Regions",
     "discoverableTaxonomySetId": "1"
   },
   {
-    "id": "1",
+    "id": "95",
     "definition": "95 - Stock Exchanges",
     "discoverableTaxonomySetId": "1"
   },
   {
-    "id": "1",
+    "id": "96",
     "definition": "96 - Currencies",
     "discoverableTaxonomySetId": "1"
   },
   {
-    "id": "1",
+    "id": "97",
     "definition": "97 - Languages",
     "discoverableTaxonomySetId": "1"
   },
   {
-    "id": "1",
+    "id": "98",
     "definition": "98 - General Purpose Contact Information",
     "discoverableTaxonomySetId": "1"
   },
   {
-    "id": "1",
+    "id": "99",
     "definition": "99 - XBRL Document Information",
     "discoverableTaxonomySetId": "1"
   }
