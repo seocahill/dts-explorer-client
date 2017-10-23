3a37c771859e6692175618bcfaaddf9b3e23273e
Seosamh Cahill <seo.cahill@gmail.com>
2017-10-23 23:14:00 +0200

    Fix Acceptance test remove helper

--- tests/acceptance/search-element-and-view-dimensions-test.js
+++ tests/acceptance/search-element-and-view-dimensions-test.js
@@ -1,7 +1,6 @@
 /* global server  */
 import { test } from 'qunit';
 import moduleForAcceptance from 'dts-explorer/tests/helpers/module-for-acceptance';
-import testSelector from 'ember-test-selectors';
 
 moduleForAcceptance('Acceptance | search NameThirdPartyAgent element and view dimensions', {
   afterEach() {
@@ -13,41 +12,33 @@ test('search element with dimensions and drill down', function(assert) {
   server.loadFixtures();
   visit('/discoverable-taxonomy-sets/1/role-types/1');
   andThen(() => assert.equal(currentURL(), '/discoverable-taxonomy-sets/1/role-types/1/presentation-nodes'));
-  click('a:contains("EntityInformationHeading")');
-  fillIn(testSelector('search-input'), "name");
-  triggerEvent(testSelector('search-input'), 'keyup');
+  click('a:contains("Entity information heading")');
+  fillIn('[data-test-search-input]', "third");
+  triggerEvent('[data-test-search-input]', 'keyup');
   click('a:contains("NameThirdPartyAgent")');
-  andThen(() => assert.equal(find(testSelector('element-properties')).text(), 'NameThirdPartyAgent'));
-
-  click('a:contains("dimensions")');
+  andThen(() => assert.equal(find('h5').text(), 'Properties'));
+  
+  click('a:contains("Dimensions")');
   click('[data-test-dimension-nodes] > a:contains("NameThirdPartyAgent")');
-  andThen(() => assert.equal(find('[data-test-dimension-nodes] > a:first').text(), 'DimensionsParent-ThirdPartyAgents (choice required)'));
-
-  click('[data-test-dimension-nodes] > a');
-  andThen(() => assert.equal(find('[data-test-dimension-nodes] > a:first').text(), 'ThirdPartyAgentsHypercube (choice required)'));
-  click('[data-test-dimension-nodes] > a');
+  andThen(() => assert.equal(find('[data-test-dimension-nodes] > a:last').text(), 'Dimensions parent third party agents (choice required)'));
+  click('[data-test-dimension-nodes] > a:last');
+  andThen(() => assert.equal(find('[data-test-dimension-nodes] > a:last').text(), 'Third party agents hypercube (choice required)'));
+  click('[data-test-dimension-nodes] > a:last');
   andThen(() => {
-    assert.equal(find('[data-test-dimension-nodes] > a:nth(5)').text(), 'tries_CountriesDimension (has default)');
+    assert.equal(find('[data-test-dimension-nodes] > a:nth(5)').text(), 'Third party agent type dimension (choice required)');
   });
   
+  click('[data-test-dimension-nodes] > a:contains("Third party agent type dimension")');
+  click('[data-test-dimension-nodes] > a:contains("Dimension members identifying type third party agent heading")');
+  andThen(() => assert.equal(find('[data-test-dimension-nodes] > a').length, 9));
 
-  click('[data-test-dimension-nodes] > a:contains("ThirdPartyAgentTypeDimension")');
-  click('[data-test-dimension-nodes] > a:contains("DimensionMembersIdentifyingTypeThirdPartyAgentHeading")');
-  andThen(() => assert.equal(find('[data-test-dimension-nodes] > a').length, 8));
-
-  click('[data-test-dimension-nodes] > a:contains("EntityBankers")');
-  click('a:contains("EntityBankers")');
+  click('[data-test-dimension-nodes] > a:contains("Entity bankers")');
   andThen(() => {
-    assert.equal(find(testSelector('element', 'name')).text(), "name EntityBankers");
-    assert.equal(find(testSelector('element', 'type')).text(), "type uk-types:domainItemType");
-    assert.equal(find(testSelector('element', 'periodType')).text(), "periodType duration");
-    assert.equal(find(testSelector('element', 'substitutionGroup')).text(), "substitutionGroup xbrli:item");
-    assert.equal(find(testSelector('element', 'nillable')).text(), "nillable true");
-    assert.equal(find(testSelector('element', 'abstract')).text(), "abstract true");
+    assert.equal(find('[data-test-element="name"]').text(), "name: EntityBankers");
+    assert.equal(find('[data-test-element="type"]').text(), "type: uk-types:domainItemType");
+    assert.equal(find('[data-test-element="periodType"]').text(), "periodType: duration");
+    assert.equal(find('[data-test-element="substitutionGroup"]').text(), "substitutionGroup: xbrli:item");
+    assert.equal(find('[data-test-element="nillable"]').text(), "nillable: true");
+    assert.equal(find('[data-test-element="abstract"]').text(), "abstract: true");
   });
-  click('a:contains("close")');
-  andThen(() => findWithAssert(testSelector('dimension-nodes')));
-
-  click(testSelector('dimension-parent'));
-  andThen(() => assert.equal(find(testSelector('dimension-node')).text(), 'DimensionMembersIdentifyingTypeThirdPartyAgentHeading'));
 });
